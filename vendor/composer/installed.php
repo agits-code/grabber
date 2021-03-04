1bc68fe3459670f092541ca54dddbb6018ebaa84
agits-code <nrrqpdMauv27012020>
2021-03-04 14:50:37 +0100

    bo back .....1

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'da4a38c495fa1cbe97ea7f2ca909124ed69a8986',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'da4a38c495fa1cbe97ea7f2ca909124ed69a8986',
+    ),
+  ),
+);
